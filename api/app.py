7e9d110dc5af618aaac78c2106331e6cf1da91eb
abdoneverland <ch.abdel.news@gmail.com>
2024-05-19 23:40:53 +0100

    simple rest api( age calculation)

--- api/app.py
+++ api/app.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
 """doc """
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello_hbnb():
+def route_home():
     """display home"""
-    return "f starting"
+    print("route home reached")
+    return render_template('index.html')
+
+
+
+@app.route("/post", methods=['POST'])
+def route_hadle_post():
+    print("post endpoint reached")
+    if request.method == 'POST':
+        print(f"------json data------{request.get_data()}")
+        data= request.get_json()
+        print(data)
+        print(data["input"])
+        print("------end------")
+    di ={"age":(2024- int(data["input"]))}
+    m= json.dumps(di)
+    print(f"sending m =## {m} ##")
+    return jsonify(di)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",port=5000,debug=False)
